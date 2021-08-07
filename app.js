a4264b7fdac212b333fcc932bb205248639b7a4d
johnfr14 <jonathan.alyra@gmail.com>
2021-08-07 19:00:43 +0200

    readme

--- app.js
+++ app.js
@@ -8,50 +8,12 @@ require('dotenv').config()
 
 const app = express()
 
-// A middle for checking if an api key is provided by the user
-// If an api key is provided in the authorization header field then
-// the api key is attached to the req object
-const getApiKey = async (req, res, next) => {
-  const apiKey = req.headers.authorization
-  if (!apiKey) {
-    res.status(403).json({
-      status: 'fail',
-      data: { apiKey: 'No api key in Authorization header' },
-    })
-  } else {
-    req.apiKey = apiKey.replace('Bearer ', '').trim()
-    next()
-  }
-}
-
-// A middleware for checking if an api key is valid
-// and is still active.
-// if Ok the id of the user performing the request is attached to the req object.
-
-const validateApiKey = async (req, res, next) => {
-  try {
-    const result = await db.getUserByApiKey(req.apiKey)
-    // Check if user is active
-    // check if null result then not found
-    if (!result || !result.active) {
-      res.status(403).json({ status: 'fail', data: { key: 'Invalid api key' } })
-    } else {
-      req.userId = result.id
-      next()
-    }
-  } catch (e) {
-    console.log(e)
-    res.status(500).json({ code: 'error', message: 'Internal server error' })
-  }
-}
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 });
-
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false })) // to support URL-encoded bodies
@@ -69,29 +31,6 @@ app.get('/db', async (req, res) => {
       res.send("Error " + err);
     }
   })
-
-app.post('/register', async (req, res) => {
-  const username = req.body.username
-  const email = req.body.email
-  const password = req.body.password
-  try {
-    const result = await db.register(username, email, password)
-    res.json({
-      status: 'success',
-      data: { id: result.id, key: result.apiKey.key },
-    })
-  } catch (e) {
-    if (e.status === 'fail') {
-      res.status(400).json({ status: e.status, data: e.dataError })
-    } else {
-      // e.status === 50X
-      res.status(500).json({ status: e.status, message: e.message })
-    }
-  }
-})
-
-app.use(getApiKey)
-app.use(validateApiKey)
 
 app.get('/user_by_id/:userId', async (req, res) => {
   let userId = req.params.userId
